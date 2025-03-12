7988ec37a3ac0346f8536a17e2452d764ff7f04a
Rimsoo <abdeldebeche@gmail.com>
2025-03-13 00:17:16 +0100

    Used preprocessor def for linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include "Core.h"
-#include "GtkUi.h"
 #include <config.h>
 #include <cstdlib>
 #include <opencv2/dnn.hpp>
 #include <opencv2/opencv.hpp>
 #include <sys/stat.h>
 
+#ifdef IS_LINUX
+#include "GtkUi.h"
+#endif
+
 int main(int argc, char *argv[]) {
 
   Core core;
-
+#ifdef IS_LINUX
   GtkUi ui(core);
+#endif
 
   core.start();
 
